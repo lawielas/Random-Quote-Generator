bca1493c7f7e9c439c40a3a8de74d404cfffa6b5
lawielas <lawie.aminu@yahoo.com>
2023-09-04 14:44:22 +0100

    this is the first commit without any code

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="wrapper">
+        <div id="quote-box">
+          <p id="text"></p>
+          <p id="author"></p>
+          <button id="new-quote"></button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
