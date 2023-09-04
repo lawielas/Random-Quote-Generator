6365be35667bc8e59a3952f32e583b86abba4f7c
lawielas <lawie.aminu@yahoo.com>
2023-09-04 15:03:27 +0100

    The quote api has been set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+
+  useEffect(() => {
+    const fetchQuote = () => {
+      fetch('https://api.quotable.io/quotes/random')
+      .then(res => {
+        return res.json()
+      })
+      .then((items) => {
+        items.map((item) => {
+          setQuote(item.content)
+          setAuthor(item.author)
+        })
+        // setQuote(data.contne)
+      })
+    }
+    fetchQuote()
+  }, [])
+
+  const handleClick = () => {
+    console.log(quote)
+    console.log(author)
+  }
 
   return (
     <>
       <div id="wrapper">
         <div id="quote-box">
-          <p id="text"></p>
-          <p id="author"></p>
-          <button id="new-quote"></button>
+          <p id="text">{quote}</p>
+          <p id="author">{author}</p>
+          <button id="new-quote" onClick={handleClick}></button>
         </div>
       </div>
     </>
